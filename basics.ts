962fcfffe99aa3c7a61460724f8feef478f2a322
jugglingdev <kaylamarie1785@gmail.com>
2023-09-23 17:13:39 -0500

    Completed 'Type Inference'

--- basics.ts
+++ basics.ts
@@ -36,3 +36,11 @@ let people: {
     name: string;
     age: number;
 }[];
+
+// Type inference
+
+let course = 'React - The Complete Guide';
+
+// course = 12341;  
+// TypeScript infers that this should be a string
+
