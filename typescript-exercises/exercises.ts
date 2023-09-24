73cd00d04dc6ba4c940e80a3d010b129aaa4ecb8
jugglingdev <kaylamarie1785@gmail.com>
2023-09-23 23:39:13 -0500

    Completed Exercise 3

--- typescript-exercises/exercises.ts
+++ typescript-exercises/exercises.ts
@@ -11,3 +11,27 @@ console.log(totalCourses);  // 4
 let subjects: string[];
 let bootcampStudent: [string, number];
 
+// Exercise 3: Interfaces
+
+interface Vehicle {
+    brand: string;
+    speed: number;
+
+    accelerate(): void;
+    brake(): void;
+}
+
+const car: Vehicle = {
+    brand: 'Toyota',
+    speed: 120,
+    accelerate(): void {
+        console.log(`The ${this.brand} is accelerating`);
+    },
+    brake(): void {
+        console.log(`The ${this.brand} is braking`);
+    }
+}
+
+car.accelerate();  // The Toyota is accelerating
+car.brake();  // The Toyota is braking
+
