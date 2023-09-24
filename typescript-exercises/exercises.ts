8a77335a9a2c8a835d4cf430061c9bc664d666d7
jugglingdev <kaylamarie1785@gmail.com>
2023-09-23 23:05:04 -0500

    Restructured repo

---
+++ typescript-exercises/exercises.ts
@@ -0,0 +1,19 @@
+// Exercise 1: Basic Types
+
+
+
+// Exercise 2: Arrays & Tuples
+
+
+
+// Exercise 3: Interfaces
+
+
+
+// Exercise 4: Functions & Types
+
+
+
+// Exercise 5: Classes with TypeScript
+
+
