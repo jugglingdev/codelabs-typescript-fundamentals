8cac745b00a1a97858f6d120439749a74571aa25
jugglingdev <kaylamarie1785@gmail.com>
2023-09-23 23:38:52 -0500

    Completed Exercise 2

--- typescript-exercises/exercises.ts
+++ typescript-exercises/exercises.ts
@@ -6,3 +6,8 @@ const totalCourses: number = 4;
 console.log(studentName); // Kayla
 console.log(totalCourses);  // 4
 
+// Exercise 2: Arrays & Tuples
+
+let subjects: string[];
+let bootcampStudent: [string, number];
+
